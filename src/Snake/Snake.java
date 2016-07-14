2255f34c5e8a73ec8e5b232e184d8ef5f191da0d
nianhuo5678 <nianhuo5678@163.com>
2016-07-14 14:00:10 +0800

    1.添加围墙 2.判断游戏结束弹窗

--- src/Snake/Snake.java
+++ src/Snake/Snake.java
@@ -13,7 +13,7 @@ public class Snake  extends JFrame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Snake snakeFrame = new Snake();
-		snakeFrame.setSize(300, 300);
+		snakeFrame.setSize(320, 350);
 		snakeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		snakeFrame.setVisible(true);
 	}
--- src/Snake/SnakeBlock.java
+++ src/Snake/SnakeBlock.java
@@ -12,17 +12,27 @@ import java.awt.event.KeyListener;
 
 public class SnakeBlock extends JPanel implements KeyListener {
 	
-	int x,y;
+	int x = 150,y = 150;
 	String direction = "right";
+	private static final int BLOCK_SIZE = 10;
+	
 
     public SnakeBlock() {
         Timer timer = new Timer(1000, new TimerListener());
         timer.start();
 	}
-    
+    @Override
 	public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.fillRect( 150 + x, 150 + y, 10, 10 );
+        for (int i = 0; i < 30; i++)
+        {
+        	for (int j = 0; j < 30; j++) {
+        		if (i == 0 || j == 0 || i == 29 || j == 29) {
+        			g.fillRect( (i * BLOCK_SIZE), (j * BLOCK_SIZE), 10, 10 );
+        		}
+        	}
+        }
+        g.fillRect( x, y, 10, 10 );
     }
 	@Override
 	public void keyTyped(KeyEvent e) {
@@ -58,6 +68,11 @@ public class SnakeBlock extends JPanel implements KeyListener {
         }
 	}
 
+	public void die() {
+		System.out.println("you lost");
+		JOptionPane.showMessageDialog(null, "GAME OVER");
+	}
+	
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
@@ -65,23 +80,43 @@ public class SnakeBlock extends JPanel implements KeyListener {
 	}
 
 	public void left() {
-		x = x - 1;
-		repaint();
+		if ( (x - BLOCK_SIZE) <= 0 ) {
+			die();
+		}
+		else {
+			x = x - BLOCK_SIZE;
+			repaint();	
+		}
 	}
 	
 	public void right() {
-		x = x + 1;
-		repaint();
+		if ( (x + BLOCK_SIZE) >= 290) {
+			die();
+		}
+		else {
+			x = x + BLOCK_SIZE;
+			repaint();
+		}
 	}
 	
 	public void up() {
-		y = y - 1;
-		repaint();
+		if ( (y - BLOCK_SIZE) <= 0) {
+			die();
+		}
+		else {
+			y = y - BLOCK_SIZE;
+			repaint();
+		}
 	}
 	
 	public void down() {
-		y = y + 1;
-		repaint();
+		if ( (y + BLOCK_SIZE) >= 290) {
+			die();
+		}
+		else {
+			y = y + BLOCK_SIZE;
+			repaint();
+		}
 	}
 	
     class TimerListener implements ActionListener {
